ec5325007ed8f7d0c5cf619f5f1002f897ade13c
JoseSembrera <143567473+JoseSembrera@users.noreply.github.com>
2023-09-16 20:50:57 -0500

    Create ejercicio3.java

---
+++ ejercicio3.java
@@ -0,0 +1,9 @@
+public class Main 
+{
+    public static void main(String[] args) {
+        double euros = 100.0; 
+        double tasaConversion = 166.386;
+        double pesetas = euros * tasaConversion;
+        System.out.println(euros + " euros son equivalentes a " + pesetas + " pesetas.");
+    }
+}

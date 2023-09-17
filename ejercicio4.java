c389fb55091bb1ed92d3730b86a99ee030a5b29a
JoseSembrera <143567473+JoseSembrera@users.noreply.github.com>
2023-09-17 09:44:53 -0500

    Create ejercicio4.java

---
+++ ejercicio4.java
@@ -0,0 +1,8 @@
+public class Main 
+{
+    public static void main(String[] args) {
+        double soles = 50; 
+        double tasaConversion = 0.22;
+        double euros = soles * tasaConversion;
+        System.out.println(soles + " soles son equivalentes a " + euros + " euros.");
+    }

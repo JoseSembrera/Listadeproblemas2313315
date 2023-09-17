005c26192bffa8dc98f96831a3f409c877205136
JoseSembrera <143567473+JoseSembrera@users.noreply.github.com>
2023-09-16 23:57:11 -0500

    Update ejercicio2.java

--- ejercicio2.java
+++ ejercicio2.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        String nombre = "Juan Pérez";
-        String direccion = "123 Calle Principal";
-        String telefono = "555-123-4567";
+        String nombre = "Jose Sembrera";
+        String direccion = "Mz S lt13 barrio 4 sector 2 Urb Pachacamac";
+        String telefono = "933-374-194";
 
         System.out.println("Nombre: " + nombre);
         System.out.println("Dirección: " + direccion);

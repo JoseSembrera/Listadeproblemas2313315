eab2d5c1c89409ba6e604a898faa2357528f25f4
JoseSembrera <143567473+JoseSembrera@users.noreply.github.com>
2023-09-17 00:37:19 -0500

    Update ejercicio3.java

--- ejercicio3.java
+++ ejercicio3.java
@@ -1,13 +1,9 @@
-import java.util.Scanner;
-
-public class Main {
+public class Main 
+{
     public static void main(String[] args) {
-        Scanner Scanner = new Scanner(System.in);
-        System.out.print("Ingresa la cantidad en euros a convertir: ");
-        double cantidadEuros = Scanner.nextDouble();
-        double valorEuroEnPesetas = 166.386;
-        double cantidadPesetas = cantidadEuros * valorEuroEnPesetas;
-        System.out.println(cantidadEuros + " euros son equivalentes a " + cantidadPesetas + " pesetas.");
+        double euros = 20; 
+        double tasaConversion = 166.386;
+        double pesetas = euros * tasaConversion;
+        System.out.println(euros + " euros son equivalentes a " + pesetas + " pesetas.");
     }
 }
-

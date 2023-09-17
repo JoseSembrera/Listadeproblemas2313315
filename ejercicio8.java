841db8e9c4e06a30e823e4eda588898861aa0d0a
JoseSembrera <143567473+JoseSembrera@users.noreply.github.com>
2023-09-17 11:37:04 -0500

    Create ejercicio8.java

---
+++ ejercicio8.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+    public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    System.out.print("Introduce la hora (solo las horas, sin minutos): ");
+    int hora = scanner.nextInt();
+    if (hora >= 6 && hora <= 12) {
+    System.out.println("Buenos días");
+    } 
+    else if (hora >= 13 && hora <= 20) {
+    System.out.println("Buenas tardes");
+    } 
+    else if ((hora >= 21 && hora <= 24) || (hora >= 0 && hora <= 5)) {
+    System.out.println("Buenas noches");
+    } 
+    else {
+    System.out.println("Hora no válida");
+    }
+}
+    }
